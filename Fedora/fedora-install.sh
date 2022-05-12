dd1f03221b750a339bb13ae4db5503ccdd1fa5a5
Caleb Williams <49931974+cayubweeums@users.noreply.github.com>
2022-05-12 18:13:24 -0500

    feat: adds firefox pwa install

--- Fedora/fedora-install.sh
+++ Fedora/fedora-install.sh
@@ -158,6 +158,20 @@ curl -s "https://raw.githubusercontent.com/saint-13/Linux_Dynamic_Wallpapers/mai
 # --------------------------------------
 
 
+# Firefox Progressive Web App Install
+# --------------------------------------
+# Import GPG key and enable the repository
+sudo rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
+echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
+
+# Update DNF cache
+sudo dnf -q makecache -y --disablerepo="*" --enablerepo="firefoxpwa"
+
+# Install the package
+sudo dnf install firefoxpwa
+# --------------------------------------
+
+
 echo -ne "
 -------------------------------------------------------------------------
                           ZSH config
