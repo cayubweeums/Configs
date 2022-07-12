966e5d3775aa1a714e8d8e5be4dbefbdcb0d267b
cayubweeums <farmingtoncardinals24@gmail.com>
2022-07-11 21:43:20 -0500

    feat: adds new packages and scripts to fedora install

--- Fedora/fedora-install.sh
+++ Fedora/fedora-install.sh
@@ -33,25 +33,32 @@ printf "Done!\n\n"
 
 echo -ne "
 -------------------------------------------------------------------------
-                            Install Packages
+                          Set up applications
 -------------------------------------------------------------------------
 "
 
-sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 
-sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
+sudo printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
-printf "Do you want to make Atom the default GUI editor?\n"
+sudo dnf install codium -y
+
+printf "Do you want to make VScodium the default GUI editor?\n"
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) xdg-mime default atom.desktop text/plain; break;;
+		Yes ) xdg-mime default vscodium text/plain; break;;
 		No ) break;;
 	esac
 done
 
 cd $dir
 
-# Set up dnf config
+
+echo -ne "
+-------------------------------------------------------------------------
+                            Config DNF
+-------------------------------------------------------------------------
+"
 echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
 echo "minrate=10k" | sudo tee -a /etc/dnf/dnf.conf
 echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
@@ -61,6 +68,12 @@ printf "Enabling RPM Fusion\n"
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 printf "Done!\n\n"
 
+
+echo -ne "
+-------------------------------------------------------------------------
+                            Install Packages
+-------------------------------------------------------------------------
+"
 printf "Installing packages"
 input="$dir/pkg-files/fedora-pkgs.txt"
 while read -r line
@@ -69,43 +82,43 @@ do
   sudo dnf -y install ${line}
 done < "$input"
 
-# Misc Packages that require extra steps
-# --------------------------------------
 
+echo -ne "
+-------------------------------------------------------------------------
+                            Misc Packages
+-------------------------------------------------------------------------
+"
 sudo dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf -y groupupdate sound-and-video
 
-# --------------------------------------
 
 
 # Laptop Specific Packages
 # --------------------------------------
-
-printf "Is this a laptop install?\n"
-select yn in "Yes" "No"; do
-	case $yn in
-		Yes ) sudo dnf -y install powertop tuned-utils;
-    sudo systemctl start tuned
-    printf "tuned recommendation: "
-    tuned-adm recommend
-    echo "SUBSYSTEM==\"power_supply\", ATTR{online}==\"0\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile laptop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
-    echo "SUBSYSTEM==\"power_supply\", ATTR{online}==\"1\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile desktop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
-    echo "SUBSYSTEM==\"power_supply\", ATTR{status}==\"Discharging\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile laptop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
-    echo "SUBSYSTEM==\"power_supply\", ATTR{status}!=\"Discharging\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile desktop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
-    sudo udevadm control --reload-rules && sudo udevadm trigger
-    sudo systemctl enable tuned
-    break;;
-		No ) break;;
-	esac
-done
-
+# printf "Is this a laptop install?\n"
+# select yn in "Yes" "No"; do
+# 	case $yn in
+# 		Yes ) sudo dnf -y install powertop tuned-utils;
+#     sudo systemctl start tuned
+#     printf "tuned recommendation: "
+#     tuned-adm recommend
+#     echo "SUBSYSTEM==\"power_supply\", ATTR{online}==\"0\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile laptop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
+#     echo "SUBSYSTEM==\"power_supply\", ATTR{online}==\"1\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile desktop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
+#     echo "SUBSYSTEM==\"power_supply\", ATTR{status}==\"Discharging\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile laptop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
+#     echo "SUBSYSTEM==\"power_supply\", ATTR{status}!=\"Discharging\", RUN+=\"`whereis tuned-adm | cut -d' ' -f2` profile desktop\"" | sudo tee --append /etc/udev/rules.d/powersave.rules
+#     sudo udevadm control --reload-rules && sudo udevadm trigger
+#     sudo systemctl enable tuned
+#     break;;
+# 		No ) break;;
+# 	esac
+# done
 # --------------------------------------
+
 
 # Flatpak install
 # --------------------------------------
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.
-
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.github.tchx84.Flatseal org.onlyoffice.desktopeditors com.github.muriloventuroso.easyssh  -y
 # --------------------------------------
 
 
@@ -116,14 +129,13 @@ sudo chmod +x anaconda.sh
 ./anaconda.sh
 # --------------------------------------
 
+
 # Gotop Install
 # --------------------------------------
-
 wget https://github.com/xxxserxxx/gotop/releases/download/v4.1.3/gotop_v4.1.3_linux_amd64.rpm
 sudo rpm -i gotop_v4.1.3_linux_amd64.rpm
 curl -O -L https://raw.githubusercontent.com/xxxserxxx/gotop/master/fonts/Lat15-VGA16-braille.psf
 setfont Lat15-VGA16-braille.psf
-
 # --------------------------------------
 
 export PATH=$PATH:~/.local/bin
@@ -138,7 +150,6 @@ mkdir ~/athena-server
 
 # GTK Themeing
 # --------------------------------------
-
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
 cd Tela-circle-icon-theme/
 ./install.sh -a -d ~/.icons
@@ -148,7 +159,6 @@ cd $dir
 git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
 cd Graphite-gtk-theme/
 ./install.sh -d ~/.themes --tweaks rimless
-
 # --------------------------------------
 
 
@@ -161,14 +171,14 @@ curl -s "https://raw.githubusercontent.com/saint-13/Linux_Dynamic_Wallpapers/mai
 # Firefox Progressive Web App Install
 # --------------------------------------
 # Import GPG key and enable the repository
-sudo rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
-echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
+# sudo rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
+# echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
 
 # Update DNF cache
-sudo dnf -q makecache -y --disablerepo="*" --enablerepo="firefoxpwa"
+# sudo dnf -q makecache -y --disablerepo="*" --enablerepo="firefoxpwa"
 
 # Install the package
-sudo dnf install firefoxpwa
+# sudo dnf install firefoxpwa
 # --------------------------------------
 
 
@@ -198,36 +208,36 @@ gem install colorls
 
 sudo touch ~/.smbcreds
 
-echo -ne "
--------------------------------------------------------------------------
-                          Linux surface install
--------------------------------------------------------------------------
-"
+# echo -ne "
+# -------------------------------------------------------------------------
+#                           Linux surface install
+# -------------------------------------------------------------------------
+# "
 
-# Surface Specific kernel
-# --------------------------------------
-sudo dnf config-manager \
-    --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+# # Surface Specific kernel
+# # --------------------------------------
+# sudo dnf config-manager \
+#     --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
-sudo dnf -y install --allowerasing kernel-surface iptsd libwacom-surface
-sudo systemctl enable iptsd
+# sudo dnf -y install --allowerasing kernel-surface iptsd libwacom-surface
+# sudo systemctl enable iptsd
 
-echo "[Unit]" | sudo tee -a /etc/systemd/system/default-kernel.path
-echo "Description=Fedora default kernel updater" | sudo tee -a /etc/systemd/system/default-kernel.path
-echo "[Path]" | sudo tee -a /etc/systemd/system/default-kernel.path
-echo "PathChanged=/boot" | sudo tee -a /etc/systemd/system/default-kernel.path
-echo "[Install]" | sudo tee -a /etc/systemd/system/default-kernel.path
-echo "WantedBy=default.target" | sudo tee -a /etc/systemd/system/default-kernel.path
+# echo "[Unit]" | sudo tee -a /etc/systemd/system/default-kernel.path
+# echo "Description=Fedora default kernel updater" | sudo tee -a /etc/systemd/system/default-kernel.path
+# echo "[Path]" | sudo tee -a /etc/systemd/system/default-kernel.path
+# echo "PathChanged=/boot" | sudo tee -a /etc/systemd/system/default-kernel.path
+# echo "[Install]" | sudo tee -a /etc/systemd/system/default-kernel.path
+# echo "WantedBy=default.target" | sudo tee -a /etc/systemd/system/default-kernel.path
 
-echo "[Unit]" | sudo tee -a /etc/systemd/system/default-kernel.service
-echo "Description=Fedora default kernel updater" | sudo tee -a /etc/systemd/system/default-kernel.service
-echo "[Service]" | sudo tee -a /etc/systemd/system/default-kernel.service
-echo "Type=oneshot" | sudo tee -a /etc/systemd/system/default-kernel.service
-echo 'ExecStart=/bin/sh -c "grubby --set-default /boot/vmlinuz*surface*"' | sudo tee -a /etc/systemd/system/default-kernel.service
+# echo "[Unit]" | sudo tee -a /etc/systemd/system/default-kernel.service
+# echo "Description=Fedora default kernel updater" | sudo tee -a /etc/systemd/system/default-kernel.service
+# echo "[Service]" | sudo tee -a /etc/systemd/system/default-kernel.service
+# echo "Type=oneshot" | sudo tee -a /etc/systemd/system/default-kernel.service
+# echo 'ExecStart=/bin/sh -c "grubby --set-default /boot/vmlinuz*surface*"' | sudo tee -a /etc/systemd/system/default-kernel.service
 
-sudo systemctl enable default-kernel.path
-sudo grubby --set-default /boot/vmlinuz*surface*
-# --------------------------------------
+# sudo systemctl enable default-kernel.path
+# sudo grubby --set-default /boot/vmlinuz*surface*
+# # --------------------------------------
 
 read -p "Enter new Hostname for this machine: " hostname
 sudo hostnamectl set-hostname $hostname
